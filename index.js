e230919d272bb56b4ce2ad8d0ce36c7d90917f59
Christian Alfoni <christianalfoni@gmail.com>
2014-08-23 17:50:57 +0200

    Added all required properties
    These are all the properties needed to be certain of no caching. Also changed resp to res, as res is more common :-)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function() {
   return noCache;
 };
-function noCache(req, resp, next) {
-  resp.setHeader('Cache-Control', 'no-cache, no-store, max-age=0');
+function noCache(req, res, next) {
+  res.setHeader('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
   next();
 }
